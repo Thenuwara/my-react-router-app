6c3c1b7cf20426b8f8c217cb4e04745771d251f7
Thenuwara <thenuwara@m-daq.com>
2026-02-11 20:02:25 +0530

    issue fix: Jest 28+ no longer includes jsdom by default,

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest/presets/js-with-babel",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
   },
