54ebe2f82dece6995f3b50cd0df3e31b0eb1c287
Thenuwara <thenuwara@m-daq.com>
2026-02-12 08:24:32 +0530

    test commit

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest/presets/js-with-babel",
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
   },
